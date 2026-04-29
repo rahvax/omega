8f370e5f50f7b3f9bea51b38d2e877bce30862c2
Gabriel Caelestis <gabriel@caelestis.one>
2026-04-29 17:42:30 -0300

    Corrigindo nome

--- src/include/cipher.h
+++ src/include/cipher.h
@@ -3,7 +3,7 @@
 #define SIZEALP 26
 
 /* Cifrar uma senha em Cifra de Cesar */
-void cesar (char *password, const int rotation);
+void caesar(char *password, const int rotation);
 /* Cifrar uma senha em Cifra Atbash */
 void atbash (char *password);
 /* Cifra uma palavra em Cifra de Vigenere */
--- src/main.c
+++ src/main.c
@@ -3,9 +3,6 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  char text[] = "gabriel";
-  char key[100] = "arrozar";
-  vigenere(text, key);
 
   return 0;
 }
